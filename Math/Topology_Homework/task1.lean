3e5bcd1f6239f8c1b14e28c37f2ccf7e6a1b90bf
flitscha <felixcampidell@gmail.com>
2025-04-15 11:23:00 +0200

    theorem completed

--- Math/Topology_Homework/task1.lean
+++ Math/Topology_Homework/task1.lean
@@ -11,7 +11,7 @@ variable [∀ i, TopologicalSpace (π i)]
 
 
 theorem homeomorphism_compact_to_hausdorff {X Y : Type*}
-[TopologicalSpace X] [TopologicalSpace Y] [CompactSpace X] [T2Space Y]
+[TopologicalSpace X] [TopologicalSpace Y] (h_compact: CompactSpace X) (h_hausdorff : T2Space Y)
 (f : X -> Y) (h_cont : Continuous f) (h_bij : Function.Bijective f) :
 ∃ g : Y → X, f ∘ g = id ∧ g ∘ f = id ∧ Continuous g := by {
 
@@ -22,41 +22,36 @@ theorem homeomorphism_compact_to_hausdorff {X Y : Type*}
 theorem uniqueness_of_product_topology {I : Type*} {π : I → Type*}
 [∀ i, TopologicalSpace (π i)] [∀ i, CompactSpace (π i)] [∀ i, T2Space (π i)]
 (T : TopologicalSpace (∀ i, π i))
---(h_a : ∀ j, @Continuous (∀ i, π i) (π j) T (by infer_instance) (fun f => f j))
---(h_b : @CompactSpace (∀ i, π i) T) :
 (h_a : ∀ j, Continuous (fun (f : ∀ i, π i) => f j))
 (h_b : CompactSpace (∀ i, π i)) :
 T = Pi.topologicalSpace := by {
 
   let α := (∀ i, π i)
 
-  apply le_antisymm
-
   -- Hinrichtung: ⊆
-  rw [Pi.topologicalSpace] -- gröbste Topologie, wo alle projektionen stetig sind
-  apply le_iInf
-  intro i
-  apply continuous_iff_le_induced.mp
-  apply h_a
+  have h_hinrichtung : T ≤ Pi.topologicalSpace := by {
+    rw [Pi.topologicalSpace] -- gröbste Topologie, wo alle projektionen stetig sind
+    apply le_iInf
+    intro i
+    apply continuous_iff_le_induced.mp
+    apply h_a
+  }
+
+  apply le_antisymm
+  apply h_hinrichtung
+
 
   -- Rückrichtung
-
   let idd : α -> α := @id α
 
   have h_id : @Continuous α α T Pi.topologicalSpace idd := by {
-    -- identity map : T -> Product is continuous
-    apply continuous_iff_le_induced.mpr
-    simp [Pi.topologicalSpace]
-    sorry
+    apply continuous_id_iff_le.mpr h_hinrichtung
   }
+  clear h_hinrichtung
 
-  have h_compact : @CompactSpace (∀ i, π i) Pi.topologicalSpace := by {
-    -- Satz von Tychonoff
-    apply Pi.compactSpace
-  }
-
-  have h_hausdorff : @T2Space (∀ i, π i) T := by {
-    apply t2Space_iff_disjoint_nhds.mpr
+  -- wir zeigen, dass die Produkttopologie ein Hausdorff-Raum ist
+  have h_hausdorff : @T2Space α Pi.topologicalSpace := by {
+    rw [t2Space_iff]
     intro x y h_not_eq
 
     -- da x ≠ y, gibt es eine Komponente i, wo x_i ≠ y_i
@@ -69,45 +64,47 @@ T = Pi.topologicalSpace := by {
     obtain ⟨i, h⟩ := h
 
     -- Wir benutzen, dass X_i haussdorff ist. Wir können x_i und y_i trennen
-    obtain ⟨U, V, hUx, hVy, h_disj⟩ := t2_separation h
+    obtain ⟨U, V, h_U_open, h_V_open, h_xU, h_yV, h_disj⟩ := t2_separation h
 
     -- Jetzt konstruieren wir die offenen Mengen im Produktraum
     let projection_i : α -> π i := (fun (f : ∀ j, π j) => f i)
     let U' : Set α := projection_i ⁻¹' U
     let V' : Set α := projection_i ⁻¹' V
-    have h_prod_open_x : @IsOpen α Pi.topologicalSpace U' := by {
-      sorry
+    letI : TopologicalSpace α := Pi.topologicalSpace -- wir arbeiten mit der Produkttopologie
+    have h_U'_open : @IsOpen α Pi.topologicalSpace U' := by {
+      apply Continuous.isOpen_preimage
+      exact continuous_apply i
+      exact h_U_open
     }
-    have h_prod_open_y : @IsOpen α Pi.topologicalSpace V' := by {
-      sorry
-    }
-
-    -- Diese mengen beinhalten x und y
-    have h_x_mem : x ∈ U' := by {
-      sorry
-    }
-    have h_y_mem : y ∈ V' := by {
-      sorry
+    have h_V'_open : @IsOpen α Pi.topologicalSpace V' := by {
+      apply Continuous.isOpen_preimage
+      exact continuous_apply i
+      exact h_V_open
     }
 
-    -- Und diese Mengen sind disjunkt
+    -- Diese Mengen sind disjunkt
     have h_disj' : Disjoint U' V' := by {
-      sorry
+      apply h_disj.preimage
     }
 
     -- jetzt können wir schließen, dass U' und V' die punkte x und y trennen.
+    use U', V'
+    exact ⟨h_U'_open, h_V'_open, h_xU, h_yV, h_disj'⟩
   }
 
-  -- Satz 5.19
-  have h : ∃ g : α → α, idd ∘ g = id ∧ g ∘ idd = id ∧ (@Continuous α α Pi.topologicalSpace T g) := by {
+  -- Wir verwenden Satz 5.19, um zu zeigen, dass die Identität ein Homöomorphismus ist.
+  have h : ∃ g : α → α, id ∘ g = id ∧ g ∘ id = id ∧ (@Continuous α α Pi.topologicalSpace T g) := by {
     apply @homeomorphism_compact_to_hausdorff α α T Pi.topologicalSpace
+    exact h_b
+    exact h_hausdorff
     apply h_id
-    sorry
+    apply Function.bijective_id
   }
 
   obtain ⟨g, h1, h2, h⟩ := h
   have hh : g = id := by {
-    sorry
+    rw [← h1]
+    simp
   }
 
   rw [hh] at h
